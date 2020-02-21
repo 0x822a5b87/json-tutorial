1168f85b93cca200167e7bdb72aabfef57b433a6
hangyudu <hangyudu@tencent.com>
2020-02-21 21:17:14 +0800

    finish tutorial06

--- tutorial06/test.c
+++ tutorial06/test.c
@@ -17,7 +17,7 @@ static int test_pass = 0;
         if (equality)\
             test_pass++;\
         else {\
-            fprintf(stderr, "%s:%d: expect: " format " actual: " format "\n", __FILE__, __LINE__, expect, actual);\
+            fprintf(stderr, "%s:%d: expect: [" format "] actual: [" format "]\n", __FILE__, __LINE__, expect, actual);\
             main_ret = 1;\
         }\
     } while(0)
@@ -352,7 +352,7 @@ static void test_parse() {
     test_parse_number();
     test_parse_string();
     test_parse_array();
-#if 0
+#if 1
     test_parse_object();
 #endif
 
@@ -366,7 +366,7 @@ static void test_parse() {
     test_parse_invalid_unicode_hex();
     test_parse_invalid_unicode_surrogate();
     test_parse_miss_comma_or_square_bracket();
-#if 0
+#if 1
     test_parse_miss_key();
     test_parse_miss_colon();
     test_parse_miss_comma_or_curly_bracket();
